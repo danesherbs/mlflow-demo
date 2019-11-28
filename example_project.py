581674ff0f0ca273dbf24e677e5bc0bf27d3d720
Dane <danesherbs@users.noreply.github.com>
2019-11-28 11:46:37 +0000

    Update example_project.py

--- example_project.py
+++ example_project.py
@@ -2,12 +2,12 @@ import mlflow
 import random
 
 
-# Can call mlflow.start_run() or wrap with `with` statement, but
-# New runs are launched under this experiment.
+# Can call mlflow.start_run() or wrap in `with` statement.
+# New runs are launched under the "Ablation study" experiment.
 mlflow.set_experiment("Ablation study")
 mlflow.start_run(run_name="M-DGI")
 
-# log_param (learning rate, num layers, ...) are for constants
+# log_param (optimiser, num layers, ...) are for constants
 mlflow.log_param("lr", random.random() * 1e-3)
 mlflow.log_param("n_layers", random.randint(100, 1000))
 mlflow.log_param("optimiser", "adam")
